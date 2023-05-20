44a9b23696e899b81a6cc84d55282ae8c6b4d50d
Basab Saha <134124512+Basab-Saha@users.noreply.github.com>
2023-05-20 17:37:22 +0530

    Update index.js

--- Part2/src/index.js
+++ Part2/src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom';
 // Traditional React rendering method
 
 ReactDOM.render(
+  
+  // To render multiple jsx elements we have to wrap them within a single <>.... <>
 
   <><h1>Basab Saha Data Scientist @ISRO</h1><p>Kolkata , Birati</p></>
 
