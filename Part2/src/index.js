3c6ab2b917001d85473a1861de56a890e68f3437
Basab Saha <134124512+Basab-Saha@users.noreply.github.com>
2023-05-20 17:35:49 +0530

    Add files via upload

---
+++ Part2/src/index.js
@@ -0,0 +1,17 @@
+import React  from 'react';
+import ReactDOM from 'react-dom';
+
+// Traditional React rendering method
+
+ReactDOM.render(
+
+  <><h1>Basab Saha Data Scientist @ISRO</h1><p>Kolkata , Birati</p></>
+
+,
+      document.getElementById("root1") );
+
+
+
+
+
+
