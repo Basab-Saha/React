1bebdf4e7bd20d7724ffd3e2036a6427bcd86d44
Basab Saha <134124512+Basab-Saha@users.noreply.github.com>
2023-05-24 20:01:34 +0530

    Delete Part10(Calculator)--import directory

--- Part10(Calculator)--import/src/Div.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-function Div(a,b){
-    return Math.floor(a/b);
-}
-
-export default Div;
--- Part10(Calculator)--import/src/Mul.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-function Mul(a,b){
-    return a*b;
-}
-
-export default Mul;
