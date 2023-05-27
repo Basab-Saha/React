71b82aee3602b0c4ef24626124f22575b95c100c
Basab Saha <134124512+Basab-Saha@users.noreply.github.com>
2023-05-27 12:26:46 +0530

    Update Imghook.jsx

--- Part13.3(Image-hook website**)/src/Imghook.jsx
+++ Part13.3(Image-hook website**)/src/Imghook.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import './index.css';
 
+
+//Description of Webapp:
+// whenever you click the changeImage button the batman image will change to superman image along with a whole new background image
+//here we are changing the state(property) of img tag (it's value)  and background-img propery of div
+
 const Imghook = () => {
   const batmanImg = "https://th.bing.com/th/id/OIP.WTSVyoQn2NJsWq8DQr7YLwHaEn?pid=ImgDet&w=1000&h=623&rs=1";
   const supermanImg = "https://th.bing.com/th/id/OIP.u8Njh0JTnyCDD54FAkkreQHaEK?pid=ImgDet&rs=1";
