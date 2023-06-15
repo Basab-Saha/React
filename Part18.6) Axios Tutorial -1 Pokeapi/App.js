9944ce803258333c116784880dfad7cb909ef0cd
Basab Saha <134124512+Basab-Saha@users.noreply.github.com>
2023-06-15 09:38:50 +0530

    Add files via upload

---
+++ Part18.6) Axios Tutorial -1 Pokeapi/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import UserBar from './UserBar'
+
+const App = () => {
+  return (
+    <div>
+        <h1 style={{textAlign:'center'}}>GitHub Users</h1>
+       <UserBar/>
+       
+    </div>
+  )
+}
+
+export default App
